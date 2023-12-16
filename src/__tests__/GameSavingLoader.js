d2aa954b146e521a305a2b8d1a277fd8581132e7
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-12-16 11:18:33 +0300

    test

--- src/__tests__/GameSavingLoader.js
+++ src/__tests__/GameSavingLoader.js
@@ -2,14 +2,9 @@ import GameSavingLoader from "../GameSavingLoader";
 
 describe('GameSavingLoader module', () => {
   test ('successful case', (done) => {
-    GameSavingLoader.load().then((saving) => {
-      // saving объект класса GameSaving
-      expect(saving).toEqual({
-        id: 9,
-        created: 1546300800,
-        userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }
-      })
-      done();
-    });
-  })
+    expect.assertions(1);
+    GameSavingLoader.load().then(saving => expect(saving).toEqual(
+      { id: 9, created: 1546300800, userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }}));
+    done();
+  });
 })
